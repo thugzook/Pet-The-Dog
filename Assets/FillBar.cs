2a3b66ddd4b5fb3a8bb008207c44e0c90ba086fa
Noah Chong <atachongnoah@gmail.com>
2020-04-26 00:45:56 -0700

    Progress Bar functionality
    "Petting" dog by clicking fills progress bar.

---
+++ Assets/FillBar.cs
@@ -0,0 +1,36 @@
+﻿/// ProgressBar code acquired from Fractal Pixels: https://fractalpixels.com/devblog/unity-2D-progress-bars
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillBar : MonoBehaviour
+{
+    // Unity UI References
+    public Slider slider;
+    public Text displayText;
+
+    // Private variables
+    private float currVal = 0f;
+    public float CurrVal
+    {
+        get {
+            return currVal;
+        }
+        set {
+            currVal = value;
+            slider.value = currVal;
+            displayText.text = (slider.value / slider.maxValue * 100).ToString("0.00") + "%";
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrVal = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/ProgressBar.cs
@@ -0,0 +1,50 @@
+﻿/// ProgressBar code acquired from Fractal Pixels: https://fractalpixels.com/devblog/unity-2D-progress-bars
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ProgressBar : FillBar
+{
+
+    // Event to invoke when the progress bar fills up
+    private UnityEvent onProgressComplete;
+
+    // Create a property to handle the slider's value
+    public new float CurrentValue
+    {
+        get
+        {
+            return base.CurrVal;
+        }
+        set
+        {
+            // If the value exceeds the max fill, invoke the completion function
+            if (value >= slider.maxValue)
+                onProgressComplete.Invoke();
+
+            // Remove any overfill (i.e. 105% fill -> 5% fill)
+            base.CurrVal = value % slider.maxValue;
+        }
+    }
+
+    void Start()
+    {
+        // Initialize onProgressComplete and set a basic callback
+        if (onProgressComplete == null)
+            onProgressComplete = new UnityEvent();
+        onProgressComplete.AddListener(OnProgressComplete);
+    }
+
+    void Update()
+    {
+        // Check to see if dog is pet
+        if (ClickManager.isPet)
+            CurrentValue += 0.0153f;
+    }
+
+    // The method to call when the progress bar fills up
+    void OnProgressComplete()
+    {
+        Debug.Log("Progress Complete");
+    }
+}
